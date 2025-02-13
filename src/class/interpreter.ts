deebe22c67710267822f5b7bd26f19872c4b26ab
Dessauw Hugo <dessauw.hugo@gmail.com>
2025-02-13 10:39:01 +0100

    Stop script on collision detected

--- src/class/interpreter.ts
+++ src/class/interpreter.ts
@@ -1,13 +1,13 @@
-import { Map } from './map'
-import { Rover } from './rover'
-import {IInterpreter, InterpreterDirection} from "../interface/interpreter.interface";
-import {Obstacle} from "./obstacle";
 import {IEtatRover} from "../interface/rover.interface";
+import {IInterpreter, InterpreterDirection} from "../interface/interpreter.interface";
+import {Rover} from "./rover";
+import {Obstacle} from "./obstacle";
+import {Map} from "./map";
 
 export class Interpreter implements IInterpreter {
   private map: Map;
   private rover: Rover;
-  private commandLine: string[]; //Exemple : ABRDDGA
+  private commandLine: string[]; // Example: ABRDDGA
   private obstacle?: Obstacle | null;
 
   public constructor(map: Map, rover: Rover, commandLine: string[], obstacle?: Obstacle | null) {
@@ -18,53 +18,55 @@ export class Interpreter implements IInterpreter {
   }
 
   Execute() {
-
     const roverState = this.rover.GetEtat();
-    const nextRoverPosition = this.rover.calculateNextPosition(true);
 
-    this.commandLine.forEach((command) => {
+    for (const command of this.commandLine) {
+      let nextRoverPosition;
+
       switch (command) {
-        case InterpreterDirection.AVANCER :
-          this.checkObstacle(nextRoverPosition.x, nextRoverPosition.y, roverState);
-          this.rover.Avancer();
-
+        case InterpreterDirection.AVANCER:
+          nextRoverPosition = this.rover.calculateNextPosition(true);
           break;
-        case InterpreterDirection.DROITE :
+        case InterpreterDirection.RECULER:
+          nextRoverPosition = this.rover.calculateNextPosition(false);
+          break;
+        case InterpreterDirection.DROITE:
           this.rover.TournerADroite();
-          this.checkObstacle(nextRoverPosition.x, nextRoverPosition.y, roverState);
-
-          this.rover.Avancer();
+          nextRoverPosition = this.rover.calculateNextPosition(true);
           break;
-        case InterpreterDirection.GAUCHE :
+        case InterpreterDirection.GAUCHE:
           this.rover.TournerAGauche();
-          this.checkObstacle(nextRoverPosition.x, nextRoverPosition.y, roverState);
-
-          this.rover.Avancer();
-          break;
-        case InterpreterDirection.RECULER :
-          this.rover.TournerADroite();
-          this.rover.TournerADroite();
-          this.checkObstacle(nextRoverPosition.x, nextRoverPosition.y, roverState);
-          this.rover.Avancer();
-
+          nextRoverPosition = this.rover.calculateNextPosition(true);
           break;
         default:
           throw new Error('Commande invalide');
       }
-    })
-  }
 
-  private checkObstacle(nextRoverPositionX: number, nextRoverPositionY: number, roverState: IEtatRover): void {
-    const obstacle: { x: number; y: number } | undefined = this.obstacle?.getObstaclePositions();
+      try {
+        this.checkObstacle(nextRoverPosition.x, nextRoverPosition.y, roverState);
 
-      //TODO SI ERREUR RENVOYER POSITION ROVER ( avec rover GetEtat position) et COORD POSITIONS
-    if (obstacle && obstacle.x === nextRoverPositionX && obstacle.y === nextRoverPositionY) {
-      const roverStateErrorMessage = `Rover position : ${roverState.GetPositionX()}, ${roverState.GetPositionY()}`;
+        if (command === InterpreterDirection.AVANCER) {
+          this.rover.Avancer();
+        } else if (command === InterpreterDirection.RECULER) {
+          this.rover.Reculer();
+        }
 
-      const obstacleErrorMessage = `Obstacle position : ${obstacle.x}, ${obstacle.y}`;
+        console.log(`Rover position : ${roverState.GetPositionX()}, ${roverState.GetPositionY()}`);
 
-      throw new Error('Collision detected : ' + roverStateErrorMessage + ' ' + obstacleErrorMessage);
+      } catch (error: any) {
+        console.error(error.message);
+        break;
+      }
     }
   }
 
+  private checkObstacle(nextRoverPositionX: number, nextRoverPositionY: number, roverState: IEtatRover): void {
+    const obstacle = this.obstacle?.getObstaclePositions();
+
+    if (obstacle && obstacle.x === nextRoverPositionX && obstacle.y === nextRoverPositionY) {
+      const roverStateErrorMessage = `Rover position : ${roverState.GetPositionX()}, ${roverState.GetPositionY()}`;
+      const obstacleErrorMessage = `Obstacle position : ${obstacle.x}, ${obstacle.y}`;
+      throw new Error('Collision detected : ' + roverStateErrorMessage + ' ' + obstacleErrorMessage);
+    }
+  }
 }
